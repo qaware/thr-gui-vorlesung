71769c16936557587e9621fffec2313a5a042abe
Stefan Langer <stefan.langer@qaware.de>
2025-06-18 22:46:18 +0200

    Added base project for exercise 11

---
+++ uebungen/11_testing/src/components/LoginForm.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref} from 'vue'
+const emit = defineEmits(['success'])
+
+const email = ref('')
+const showSuccess = ref(false);
+const login = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {method: 'POST'})
+  if (res.ok) {
+    emit('success')
+    showSuccess.value = true
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="login">
+    <label for="email">E-Mail</label>
+    <input id="email" v-model="email"/>
+    <button type="submit">Login</button>
+    <div v-if="showSuccess">Success</div>
+  </form>
+</template>
